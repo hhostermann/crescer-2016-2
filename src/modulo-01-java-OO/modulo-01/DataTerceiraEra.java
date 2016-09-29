ec67c44a74d9c5eb981872f3e25eaab596b1eb18
Unknown <hhosterman@hotmail.com>
2016-09-29 00:59:10 -0300

    fix(DataTerceiraEra): arrumar para o test

--- src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -1,10 +1,11 @@
+
 public class DataTerceiraEra
 {
     private int dia;
     private int mes;
     private int ano;
     
-   public DataTerceiraEra(int dia, int mes, int ano) { 
+    public DataTerceiraEra(int dia, int mes, int ano) { 
         this.dia = dia; 
         this.mes = mes;
         this.ano = ano;
@@ -12,12 +13,21 @@ public class DataTerceiraEra
     public int getDia (){
         return dia;
     }
-    
     public int getMes (){
         return mes;
-    }
-    
+    }    
     public int getAno (){
         return ano;
+    }
+    public boolean ehBissexto(){
+        if ((ano % 400 == 0) && (ano % 100 != 0)){
+           return true;
+        }
+        if ((ano % 4 == 0) && ( (ano % 100 != 0) || (ano % 400 == 0))){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
