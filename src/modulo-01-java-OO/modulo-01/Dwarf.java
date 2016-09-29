416a0cc222f2e99064b5b014acb82193a286461a
Unknown <hhosterman@hotmail.com>
2016-09-29 01:34:54 -0300

    fix(Dwarf): Adicionar nome e dataNascimento

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -1,8 +1,23 @@
 
 public class Dwarf{
     private int vida;
+    private String nome;
+    private DataTerceiraEra dataNascimento;
     {
         vida=110;
+    }
+    public Dwarf (String n){    // Chamando construtor debaixo 
+        nome = n;
+        dataNascimento = new DataTerceiraEra (1, 1, 1);
+    }
+    public void setNome(String n){ 
+        nome = n;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public DataTerceiraEra getDataNascimento(){
+        return dataNascimento;
     }
     public void perderVida(){
         vida -= 10;
@@ -11,4 +26,3 @@ public class Dwarf{
         return vida;
     }
 }
-
