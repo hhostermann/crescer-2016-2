61e9f1d59932c7a8460fa59e5b84fd759b4fbdad
Unknown <hhosterman@hotmail.com>
2016-09-28 23:35:06 -0300

    feat(dwarf): Criacao classe dwarf

---
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -0,0 +1,14 @@
+
+public class Dwarf{
+    private int vida;
+    {
+        vida=110;
+    }
+    public void perderVida(){
+        vida -= 10;
+    }
+    public int getVida(){
+        return vida;
+    }
+}
+
