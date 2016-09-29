8cdfbc2b3098728e9a5e1e7276e84ac06c2912ee
Unknown <hhosterman@hotmail.com>
2016-09-28 22:16:34 -0300

    feat(CestaTeste): adicionar testes

---
+++ src/modulo-01-java-OO/modulo-01/CestaTeste.java
@@ -0,0 +1,24 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CestoDeLembasTest
+{
+    @Test 
+    public void dividirCestoDoisLemba() { 
+        // Arrange 
+        CestoDeLembas cesto = new CestoDeLembas(2); 
+        // Act & Assert 
+        assertFalse(cesto.dividirPar()); 
+    } 
+    @Test 
+    public void dividirCestoQuatroLemba() { 
+        // Arrange 
+        CestoDeLembas cesto = new CestoDeLembas(4); 
+        // Act & Assert 
+        assertTrue(cesto.dividirPar()); 
+    } 
+    } 
