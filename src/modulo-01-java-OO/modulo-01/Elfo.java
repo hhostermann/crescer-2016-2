1c57560d86cd7afd620606601f7534f39601d45e
Unknown <hhosterman@hotmail.com>
2016-09-28 22:17:13 -0300

    feat(Elfo): class elfo

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -1,35 +1,33 @@
-public class Elfo { 
-    private String nome; 
-    private Item arco; 
+public class Elfo{
+    private String nome;
+    private Item arco;
     private Item flecha;
-    private Dwarf anao;
-    private int experiencia; 
-    private int contador;
-    public Elfo(String n) { 
-        nome = n; 
-        arco = new Item("Arco", 1); 
-        flecha = new Item("Flecha", 42); 
-    } 
-    public void setNome(String n) { 
-        nome = n; 
+    private int experiencia;
+    
+    public Elfo (String n) {
+        nome = n;
+        arco = new Item("Arco", 1);
+        flecha = new Item("Flecha", 42);
+    }
+    public void setNome(String n){
+        nome = n;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public Item getArco (){
+        return arco;
+    }
+    public Item getFlecha(){
+        return flecha;
+    }   
+    public int getExperiencia(){
+        return experiencia;
+    }
+    public void atirarFlecha(){
+      flecha.setQuantidade(flecha.getQuantidade()-1);
+      experiencia++;
     }  
-    public String getNome() { 
-        return nome; 
-    } 
-    public Item getArco() { 
-        return arco; 
-    }
-    public Item getFlecha() { 
-        return flecha; 
-    } 
-    public void atirarFlecha() { 
-        if (flecha.getQuantidade() >= 0 ){
-            flecha.setQuantidade(flecha.getQuantidade() - 1);
-            anao.setPontosDeVida(anao.getPontosDeVida() - 10);
-            experiencia++; 
-        }
-        else{
-            System.out.println("Acabaram as Flechas");
-        }
-    }
+    
+    
 }
