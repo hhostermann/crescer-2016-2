a5a5e198ee4a8d91dab032e0584ef2b0c8cb7c03
Unknown <hhosterman@hotmail.com>
2016-09-29 00:38:49 -0300

    feat(DataTerceiraEra): criação do arquivo e atividades 3.1 3.2

---
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -0,0 +1,23 @@
+public class DataTerceiraEra
+{
+    private int dia;
+    private int mes;
+    private int ano;
+    
+   public DataTerceiraEra(int dia, int mes, int ano) { 
+        this.dia = dia; 
+        this.mes = mes;
+        this.ano = ano;
+    } 
+    public int getDia (){
+        return dia;
+    }
+    
+    public int getMes (){
+        return mes;
+    }
+    
+    public int getAno (){
+        return ano;
+    }
+}
