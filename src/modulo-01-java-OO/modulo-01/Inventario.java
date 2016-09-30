51901dff7cdf5869fedab9636a2ffe2bbeefb20b
Unknown <hhosterman@hotmail.com>
2016-09-30 02:55:55 -0300

    feat(inventario): ARRUMAR descriçao dos itens

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -1,16 +1,23 @@
 import java.util.ArrayList; 
 public class Inventario
 {
-   private ArrayList<Item> inventario = new ArrayList<Item>();
- 
-   //public Inventario (){
-        //this.item = new Item (item);
-        //ArrayList <Item> item = new ArrayList<Item> ();
-    //}
+   private ArrayList<Item> inventario = new ArrayList<>();
+
    public void adicionarItem(Item item){
         inventario.add(item);
     }
    public void removerItem(Item item){
         inventario.remove(item);
     }
+   public ArrayList<Item> getItens(){
+       return inventario;
+    }
+   public String getDescricoesItens(){
+       for (int i = 0; i < inventario.size(); i++) { 
+            Item itemAtual = inventario.get(i); 
+            
+            System.out.println("%s, ", itemAtual.getDescricao()); // erro no .printl
+        }   
+        
+   }
 }
