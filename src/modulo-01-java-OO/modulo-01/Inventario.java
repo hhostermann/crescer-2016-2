3307f19d795b6b1e14e2194f721078f86a0f1387
Unknown <hhosterman@hotmail.com>
2016-09-30 10:23:19 -0300

    fix(inventario): arrumar tentativa GetInventario

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class Inventario
 {
    private ArrayList<Item> inventario = new ArrayList<>();
-
+    
    public void adicionarItem(Item item){
         inventario.add(item);
     }
@@ -12,12 +12,12 @@ public class Inventario
    public ArrayList<Item> getItens(){
        return inventario;
     }
-   public String getDescricoesItens(){
+   public String getDescricoesItens(String GAMBI){
        for (int i = 0; i < inventario.size(); i++) { 
-            Item itemAtual = inventario.get(i); 
-            
-            System.out.println("%s, ", itemAtual.getDescricao()); // erro no .printl
-        }   
+            Item itemAtual = inventario.get(i);
+            GAMBI = itemAtual.getDescricao();
+            System.out.println ("%s, ", itemAtual.getDescricao()); // erro no .printl
+        }   }
         
    }
-}
+
