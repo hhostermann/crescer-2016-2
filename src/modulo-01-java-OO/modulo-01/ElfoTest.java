ba93e4af4f14a0610cb3224723521c7489652dd1
Unknown <hhosterman@hotmail.com>
2016-10-01 00:00:35 -0300

    test(elfoTest): testar toString

--- src/modulo-01-java-OO/modulo-01/ElfoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoTest.java
@@ -21,70 +21,112 @@ public class ElfoTest
     @Test
     public void elfoAtiraCincoFlecha(){
         Elfo elfoTeste =  new Elfo ("Galandriel");
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
+        Dwarf dwarf = new Dwarf();
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
         
         assertEquals (5, elfoTeste.getExperiencia());
     }
     @Test
     public void elfoAtiraFlecha(){
         Elfo elfoTeste =  new Elfo ("Arwen");
-        elfoTeste.atirarFlecha();
+        Dwarf dwarf = new Dwarf();
+        elfoTeste.atirarFlecha(dwarf);
         assertEquals (1, elfoTeste.getExperiencia());
     }
     @Test
     public void elfoAtiraQuarentaCincoFlecha(){
         Elfo elfoTeste =  new Elfo ("Galandriel");
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();      
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();       
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();       
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();        
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();        
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();        
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();        
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();       
-		elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
-        elfoTeste.atirarFlecha();
+        Dwarf dwarf = new Dwarf();
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);      
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);       
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);       
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);        
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);        
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);        
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);        
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);       
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
         
         assertEquals(0, elfoTeste.getFlecha().getQuantidade()); 
         assertEquals (42, elfoTeste.getExperiencia());
+        assertEquals (-310, dwarf.getVida());
+    }
+    @Test 
+    public void elfoAtiraDwarf(){
+        //ACT
+        Elfo elfoTeste = new Elfo("Dobby");
+        Dwarf dwarf = new Dwarf();
+        elfoTeste.atirarFlecha(dwarf);
+        
+        assertEquals (100, dwarf.getVida());
+    }
+    @Test
+    public void elfoToString(){
+        Elfo elfoTeste = new Elfo("Legolas");
+        Dwarf dwarf = new Dwarf();
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.atirarFlecha(dwarf);
+        elfoTeste.toString();
+        assertEquals ("Legolas possui 38 flechas e 4 níveis de experiência.", elfoTeste.toString());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
