b312464e0b295cfd3df0509f1365aab975dea6da
Unknown <hhosterman@hotmail.com>
2016-09-30 22:29:29 -0300

    feat(elfo): atirarFlecha e experiencia

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -1,10 +1,14 @@
 
 public class Elfo
 {
-   
+  /*
+
+Criar atributo experiencia e aumentá-la a cada chamada de atirarFlecha().*/
     private String nome;
     private Item arco;
     private Item flecha;
+    private int experiencia;
+    
     public Elfo (String n) { 
         nome = n; 
         arco = new Item("Arco", 1); 
@@ -12,6 +16,12 @@ public class Elfo
     }
     public String getNome(){
         return nome;
+    }
+    public int getExperiencia(){
+        return experiencia;
+    }
+    public void atirarFlecha(){
+        experiencia++;
     }
     
 
