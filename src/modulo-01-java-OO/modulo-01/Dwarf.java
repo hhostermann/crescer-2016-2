f0463ad32c77e0028abdaa748c562eeb29900a05
Unknown <hhosterman@hotmail.com>
2016-10-01 03:41:46 -0300

    feat(Dwarf): Codigo refeito

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -1,21 +1,46 @@
 
-/*
- Permitir que Elfos atirem flechas em Dwarves (anões)
- Lembrem-se que um Dwarf nasce com 110 unidades de vida e perde 10 a cada flechada.
- */
 public class Dwarf
 {
     private int vida;
-    public Dwarf()
-    {
-        vida=110;
-    }
-    public void perdeVida()
-    {
-        vida-=10;
+    private String nome;
+    private DataTerceiraEra dataNascimento;
+    private double valorInicial = 101.0;
+    private int experiencia;
+
+    public Dwarf(String n, DataTerceiraEra dataNascimento){
+        this.nome = n;        
+        this.dataNascimento = dataNascimento;
+        this.vida=110;
     }
     public int getVida(){
         return vida;
     }
-    
+    public String getNome(){
+        return nome;
+    }
+    public int getExperiencia(){
+        return experiencia;
+    }
+    public void perdeVida(){
+        double numero = this.getNumeroSorte();
+        if (numero < 0){
+            experiencia += 2;
+        }
+        if (numero<100){
+            vida-=10;
+        }
+    }
+    public double getNumeroSorte(){
+        double valorFinal = valorInicial;
+        boolean bissexto = dataNascimento.ehBissexto();
+        if(bissexto && this.vida>=80 && this.vida<=90){
+            valorFinal *= -33.0;
+        }
+        if (!bissexto && ("Seixas".equals(this.nome) || "Meireles".equals(this.nome))){
+            valorFinal = valorInicial*33 % 100;
+        }
+        return valorFinal;
+    }
+
 }
+
