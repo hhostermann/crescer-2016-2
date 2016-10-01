d8c40d77ff595e99a9edec83fcd40641cbe5b8cd
Unknown <hhosterman@hotmail.com>
2016-09-30 22:19:02 -0300

    feat(elfo): elfo ganha arco e flecha

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -5,11 +5,10 @@ public class Elfo
     private String nome;
     private Item arco;
     private Item flecha;
-    public Elfo(String n)
-    {
-       nome = n;
-       this.arco = new Item("Arco",1);
-       this.flecha = new Item("Flecha", 42);
+    public Elfo (String n) { 
+        nome = n; 
+        arco = new Item("Arco", 1); 
+        flecha = new Item("Flecha", 42);
     }
     public String getNome(){
         return nome;
