a9ff28f9779bbf8bca7a8c8853645c4586ba4aec
Unknown <hhosterman@hotmail.com>
2016-10-02 00:07:17 -0300

    feat(Inventario): adicionar inventario e descricao de itens

---
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList; 
+ 
+public class Inventario { 
+    private ArrayList<Item> itens; 
+     
+    public Inventario() { 
+        itens = new ArrayList<>(); 
+    } 
+     
+    public ArrayList<Item> getItens() { 
+        return itens; 
+    } 
+     
+    public void adicionarItem(Item item) { 
+        itens.add(item); 
+    } 
+     
+    public void removerItem(Item item) { 
+        itens.remove(item); 
+    } 
+    public String getDescricoesItens(){
+        String resultado = "";
+        for (Item itemAtual : itens){
+            resultado += String.format("%s,", itemAtual.getDescricao());
+        }
+        return resultado.isEmpty() ?resultado : resultado.substring(0, resultado.length () -1);
+    }
+} 
+    
