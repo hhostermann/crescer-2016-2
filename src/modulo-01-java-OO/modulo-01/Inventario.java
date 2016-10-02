a8e80d4011812bc23570ed16c0de4d33f3be2284
Unknown <hhosterman@hotmail.com>
2016-10-02 16:25:16 -0300

    feat(inventario): ordenar itens

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
  
 public class Inventario { 
     private ArrayList<Item> itens; 
-     
+    private ArrayList<Item> inventario;  
     public Inventario() { 
         itens = new ArrayList<>(); 
+                inventario = new ArrayList<>(); 
     } 
      
     public ArrayList<Item> getItens() { 
@@ -24,6 +25,28 @@ public class Inventario {
             resultado += String.format("%s,", itemAtual.getDescricao());
         }
         return resultado.isEmpty() ?resultado : resultado.substring(0, resultado.length () -1);
+    }
+    public void ordenarItens(){
+        int i,u,contador;
+        Item item;
+        for (i=0; i<= itens.size(); i++){
+            int numeroMenor = itens.get(i).getQuantidade();
+            item = itens.get(i);
+                        for (u=0; i<= itens.size();i++){
+            int quantidade = itens.get(u).getQuantidade();
+                if (numeroMenor >= quantidade){
+                    numeroMenor =  quantidade; 
+                    contador = u;
+                    item =  itens.get(u);
+                    itens.remove(u);
+            }
+            }
+            inventario.add(item);
+        }
+        for (i=0; i<=inventario.size(); i++){
+            itens.add(inventario .get(i));
+        }
+            
     }
 } 
     
