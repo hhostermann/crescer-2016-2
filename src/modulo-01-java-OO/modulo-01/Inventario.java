9621438275ab8ff7b0fb07b4e5c7df87f96a6c94
Unknown <hhosterman@hotmail.com>
2016-10-02 18:08:09 -0300

    fix(Inventario):correção na descrição e deletar ordenação

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList; 
+
  
 public class Inventario { 
     private ArrayList<Item> itens; 
-    private ArrayList<Item> inventario;  
+ 
     public Inventario() { 
         itens = new ArrayList<>(); 
-                inventario = new ArrayList<>(); 
-    } 
-     
+     } 
     public ArrayList<Item> getItens() { 
         return itens; 
     } 
@@ -21,32 +20,9 @@ public class Inventario {
     } 
     public String getDescricoesItens(){
         String resultado = "";
-        for (Item itemAtual : itens){
+        for (Item itemAtual : getItens()){
             resultado += String.format("%s,", itemAtual.getDescricao());
         }
         return resultado.isEmpty() ?resultado : resultado.substring(0, resultado.length () -1);
     }
-    public void ordenarItens(){
-        int i,u,contador;
-        Item item;
-        for (i=0; i<= itens.size(); i++){
-            int numeroMenor = itens.get(i).getQuantidade();
-            item = itens.get(i);
-                        for (u=0; i<= itens.size();i++){
-            int quantidade = itens.get(u).getQuantidade();
-                if (numeroMenor >= quantidade){
-                    numeroMenor =  quantidade; 
-                    contador = u;
-                    item =  itens.get(u);
-                    itens.remove(u);
-            }
-            }
-            inventario.add(item);
-        }
-        for (i=0; i<=inventario.size(); i++){
-            itens.add(inventario .get(i));
-        }
-            
-    }
-} 
-    
+}   
