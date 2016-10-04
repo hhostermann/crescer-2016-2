61f98ded5e55765151ca796e7711208c201a2e4d
Unknown <hhosterman@hotmail.com>
2016-10-03 22:56:34 -0300

    feat(ElfoNoturno): criação da classe filho junto com os testes

---
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturnoTest.java
@@ -0,0 +1,44 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElfoNoturnoTest
+{
+     @Test
+    public void ElfoNoturnoAtiraDuasFlecha() {
+        // Arrange
+        ElfoNoturno legolas = new ElfoNoturno("Batman" , 42);
+        // Act
+       legolas.atirarFlecha(new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1)));
+       legolas.atirarFlecha(new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1)));
+    }
+    @Test
+    public void ElfoNoturnoAtiraFlechaEmUmDwarf() {
+        ElfoNoturno ElfoNoturno = new ElfoNoturno("Batman" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoNoturno.atirarFlecha(balin);
+        assertEquals(100, balin.getVida());
+    }
+    @Test
+    public void ElfoNoturnoAtiraDuasFlechaEmUmDwarf() {
+
+        ElfoNoturno ElfoNoturno = new ElfoNoturno("Batman" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoNoturno.atirarFlecha(balin);
+        ElfoNoturno.atirarFlecha(balin);
+        assertEquals(90, balin.getVida());
+    }
+    @Test
+    public void ElfoNoturnoAtiraUmaFlechaEmCadaDwarf() {
+        ElfoNoturno ElfoNoturno = new ElfoNoturno("Batman" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        Dwarf gloin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoNoturno.atirarFlecha(balin);
+        ElfoNoturno.atirarFlecha(gloin);
+        assertEquals(100, balin.getVida());
+        assertEquals(100, gloin.getVida());
+    }
+}
