3e2a585e310f5dba3fe08333bc564f6e5b7c6e46
Unknown <hhosterman@hotmail.com>
2016-10-03 22:57:04 -0300

    fest(ElfoVerde): criação da classe filho e teste

---
+++ src/modulo-01-java-OO/modulo-01/ElfoVerdeTest.java
@@ -0,0 +1,44 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+ 
+public class ElfoVerdeTest
+{
+     @Test
+    public void ElfoVerdeAtiraDuasFlecha() {
+        // Arrange
+        ElfoVerde legolas = new ElfoVerde("ElfoVerde" , 42);
+        // Act
+       legolas.atirarFlecha(new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1)));
+       legolas.atirarFlecha(new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1)));
+    }
+    @Test
+    public void ElfoVerdeAtiraFlechaEmUmDwarf() {
+        ElfoVerde ElfoVerde = new ElfoVerde("ElfoVerde" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoVerde.atirarFlecha(balin);
+        assertEquals(100, balin.getVida());
+    }
+    @Test
+    public void ElfoVerdeAtiraDuasFlechaEmUmDwarf() {
+
+        ElfoVerde ElfoVerde = new ElfoVerde("ElfoVerde" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoVerde.atirarFlecha(balin);
+        ElfoVerde.atirarFlecha(balin);
+        assertEquals(90, balin.getVida());
+    }
+    @Test
+    public void ElfoVerdeAtiraUmaFlechaEmCadaDwarf() {
+        ElfoVerde ElfoVerde = new ElfoVerde("ElfoVerde" , 42);
+        Dwarf balin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        Dwarf gloin = new Dwarf("Gimili", new DataTerceiraEra(1, 1, 1));
+        ElfoVerde.atirarFlecha(balin);
+        ElfoVerde.atirarFlecha(gloin);
+        assertEquals(100, balin.getVida());
+        assertEquals(100, gloin.getVida());
+    }
+}
