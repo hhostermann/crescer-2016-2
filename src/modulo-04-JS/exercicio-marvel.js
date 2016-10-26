1f55eab4a93e4220c5b0943098cc8c9a55433ed8
Unknown <hhosterman@hotmail.com>
2016-10-26 11:00:07 -0200

    feat(exercicio-marvel): exercico 1 e 2 ssem text

---
+++ src/modulo-04-JS/exercicio-marvel.js
@@ -0,0 +1,21 @@
+class ClassName {
+  constructor(array) {
+    this.array = array;
+  }
+ foraDaGuerraCivil() {
+    for(var i = 0 ; i < this.array.length ; i++){
+      var objeto = this.array[i];
+      var evento = objeto.events;
+      var item = evento.items;
+      var fora = false;
+      for(var j=0;j<itens.length;j++){
+      if(itens[j].name ==! 'Civil War'){
+        fora = true;
+      }
+      }
+      if(fora){
+        console.log(objeto.name);
+      }
+    }
+}
+}
