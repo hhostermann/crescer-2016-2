d947840580ac3be0d9d426acbc72f70dc6926b51
Unknown <hhosterman@hotmail.com>
2016-11-01 10:38:33 -0200

    feat(streetFighter): exercicios

---
+++ src/Modulo-05-C#/StreetFight/StreetFighter/Controllers/StreetFighterController.cs
@@ -0,0 +1,61 @@
+﻿using StreetFighter.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FichaTecnica()
+        {
+            var model = new ModelFichaTecnica();
+            model.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
+            model.DataNascimento = DateTime.Parse("12/02/1966", new CultureInfo("pt-BR")); 
+            model.Altura = 192;
+            model.Peso = 96;
+            model.Medidas = "B198, C120, Q172";
+            model.TipoSanguineo = 'B';
+            model.HabilidadesEspeciais = "Caçar, Eletricidade";
+            model.Gosta = "Frutas tropicais, Pirarucu, Sua mãe";
+            model.Desgosta = "Army ants (espécie de formiga)";
+            model.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira";
+            model.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia)";
+            model.FalaDeVitoria = "\"Ver você em ação é uma piada!\"";
+            model.SSF2Nickname = "\"A selvagem criança da natureza\"";
+            model.SFA3Nickname = "\"A animal pessoa amazônica\"";
+            model.SF4Nickname = "\"Guerreiro da selva\"";
+            model.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
+            model.SF2Stage = "Bacia do rio Amazonas (Brasil)";
+            model.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack";
+            return View(model);
+        }
+        public ActionResult Sobre()
+        {
+            var model = new ModelSobre();
+            model.PrimeiraAparicao = "Hospital de Taquara";
+            model.DataNascimento = DateTime.Parse("11/11/1990", new CultureInfo("pt-BR"));
+            model.Altura = 196;
+            model.Peso = 99;
+            model.Medidas = "sem ideia";
+            model.TipoSanguineo = "O+";
+            model.HabilidadesEspeciais = "Comer, Dormir, ver séries.";
+            model.Gosta = "BACON, BACON, BACON";
+            model.EstiloDeLuta = "Falar sobre TI (afasta 80% das pessoas)";
+            model.Origem = "Taquara - RS - Brasil";
+            model.FalaDeVitoria = "GG!!";
+            model.WowNickname = "Soobored";
+            model.SteanNickname = "hhostermann";
+            model.Estilo = "suporte, Tank";
+            return View(model);
+        }
+    }
+}
---
+++ src/Modulo-05-C#/StreetFight/StreetFighter/Models/ModelSobre.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Models
+{
+    public class ModelSobre
+    {
+        public string PrimeiraAparicao { get; set; }
+        public DateTime DataNascimento { get; internal set; }
+        public int Altura { get; set; }
+        public int peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguinio { get; set; }
+        public string HabilidadeEspecial { get; set; }
+        public string Gosta { get; set; }
+        public string EstiloDeluta { get; set; }
+        public string Origem { get; set; }
+        public string FalaDeVitoria { get; set; }
+        public string WowNickname { get; set; }
+        public string SteanNickname { get; set; }
+        public string Estilo { get; set; }
+        public int Peso { get; internal set; }
+        public string TipoSanguineo { get; internal set; }
+        public string HabilidadesEspeciais { get; internal set; }
+        public string EstiloDeLuta { get; internal set; }
+    }
+    //        	<strong>Primeira aparição:</strong>Hospital de Taquara<br>
+    //<strong>Nascimento: </strong>11 de Novembro de 1990. <br>
+    //<strong>Altura:</strong> 196cm. <br>
+    //<strong>Peso:</strong> 100kg ++. <br>
+    //<strong>Medidas:</strong>não faço ideia<br> 
+    //<strong>Tipo sanguíneo:</strong> O+ <br>
+    //<strong>Habilidades especiais:</strong> Comer, Dormir, ver séries. (não nessa ordem) <br>
+    //<strong>Gosta:</strong> BACON, BACON, BACON. <br>
+    //<strong>Estilo de luta:</strong> Falar sobre TI(afasta 80% das pessoas)<br> 
+    //<strong>Origem:</strong> Taquara-RS - Brasil. <br>
+    //<strong>Uma fala de vitória:</strong> "foi??" <br>
+    //<strong>WoW nickname:</strong> Soboreed. <br>
+    //<strong>Stean nickname:</strong> Cowabunga. <br>
+    //            <strong>Estilo preferido:</strong>Suporte, Tank. <br>
+
+}
